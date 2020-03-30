d772252dcfe0bbe4dc5d88e3355f26a93c72b91c
Eduard Saryan <eduardevops@users.noreply.github.com>
2020-03-30 14:54:19 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 set -euo pipefail
 
 # Run scripts to configure/prepare environment in runtime
-# if [[ -d "/entrypoint.d" ]] ; then
-  # for script in /entrypoint.d/* ; do
-    # if [[ -x "${script}" ]] ; then
-      # "${script}"
-    # fi
-  # done
-# fi
+if [[ -d "/entrypoint.d" ]] ; then
+  for script in /entrypoint.d/* ; do
+    if [[ -x "${script}" ]] ; then
+      "${script}"
+    fi
+  done
+fi
 
 # Run supervisord
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
